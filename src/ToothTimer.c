24580518b5417a5c83b140233d4a9df33e525c82
Christopher Loessl <cloessl+github@gmail.com>
2015-09-06 00:15:07 +0200

    Fixes: Make it compile again and fixes indent

--- src/ToothTimer.c
+++ src/ToothTimer.c
@@ -30,25 +30,25 @@ static VibePattern threeshort_pattern = {
 };
 
 struct {
-	int time; // in seconds
-	char *text;
-	VibePattern *vibe;
+  int time; // in seconds
+  char *text;
+  VibePattern *vibe;
 } msgs[10] = {
-	{  3, "Get ready", NULL },
+  {  3, "Get ready", NULL },
 
-	{ 15, "Lower right outer", &twoshort_pattern },
-	{ 15, "Lower left outer", &oneshort_pattern },
+  { 15, "Lower right outer", &twoshort_pattern },
+  { 15, "Lower left outer", &oneshort_pattern },
 
-	{ 15, "Lower left inner", &oneshort_pattern },
-	{ 15, "Lower right inner", &oneshort_pattern },
+  { 15, "Lower left inner", &oneshort_pattern },
+  { 15, "Lower right inner", &oneshort_pattern },
 
-	{  3, "Spit", &twoshort_pattern },
+  {  3, "Spit", &twoshort_pattern },
 
-	{ 15, "Upper right outer", &twoshort_pattern },
-	{ 15, "Upper left outer", &oneshort_pattern },
+  { 15, "Upper right outer", &twoshort_pattern },
+  { 15, "Upper left outer", &oneshort_pattern },
 
-	{ 15, "Upper left inner", &oneshort_pattern },
-	{ 15, "Upper right inner", &oneshort_pattern },
+  { 15, "Upper left inner", &oneshort_pattern },
+  { 15, "Upper right inner", &oneshort_pattern },
 };
 
 const int msgs_size = sizeof(msgs)/sizeof(msgs[0]);
@@ -63,56 +63,56 @@ exit_callback(void *data)
 static void 
 timer_callback(void *data)
 {
-	
-	if (nextstep > msgs_size-1)
-	{
-		s_step_layer[0] = 0;
-		s_nextstep_layer[0] = 0;
-		snprintf(s_countdown_layer, sizeof(s_countdown_layer), "Done!");
-		layer_mark_dirty(text_layer_get_layer(step_layer));
-		layer_mark_dirty(text_layer_get_layer(nextstep_layer));
-		layer_mark_dirty(text_layer_get_layer(countdown_layer));
-		vibes_enqueue_custom_pattern(threeshort_pattern);
+  
+  if (nextstep > msgs_size-1)
+  {
+    s_step_layer[0] = 0;
+    s_nextstep_layer[0] = 0;
+    snprintf(s_countdown_layer, sizeof(s_countdown_layer), "Done!");
+    layer_mark_dirty(text_layer_get_layer(step_layer));
+    layer_mark_dirty(text_layer_get_layer(nextstep_layer));
+    layer_mark_dirty(text_layer_get_layer(countdown_layer));
+    vibes_enqueue_custom_pattern(threeshort_pattern);
     tooth_timer = app_timer_register(5*60*1000 /* 5 minutes */, exit_callback, NULL);
-		return;
-	}
+    return;
+  }
 
-	tooth_timer = app_timer_register(1000 /* milliseconds */, timer_callback, NULL);
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "timer_callback nextstep:%d, nextstep_countdown:%d", nextstep, nextstep_countdown);
-	if (nextstep_countdown <= 0)
-	{
-		nextstep_countdown = msgs[nextstep].time;
-		if (msgs[nextstep].vibe)
-			vibes_enqueue_custom_pattern(*msgs[nextstep].vibe);
-		snprintf(s_step_layer, sizeof(s_step_layer), "%s", msgs[nextstep].text);
-		APP_LOG(APP_LOG_LEVEL_DEBUG, "tc: new step: %s (%d)", msgs[nextstep].text, nextstep_countdown);
-		if (nextstep < msgs_size-1)
-			snprintf(s_nextstep_layer, sizeof(s_nextstep_layer), "Next:\n%s", msgs[nextstep+1].text);
-		else
-			snprintf(s_nextstep_layer, sizeof(s_nextstep_layer), "Next:\nDone!");
-		layer_mark_dirty(text_layer_get_layer(step_layer));
-		layer_mark_dirty(text_layer_get_layer(nextstep_layer));
-	}
+  tooth_timer = app_timer_register(1000 /* milliseconds */, timer_callback, NULL);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "timer_callback nextstep:%d, nextstep_countdown:%d", nextstep, nextstep_countdown);
+  if (nextstep_countdown <= 0)
+  {
+    nextstep_countdown = msgs[nextstep].time;
+    if (msgs[nextstep].vibe)
+      vibes_enqueue_custom_pattern(*msgs[nextstep].vibe);
+    snprintf(s_step_layer, sizeof(s_step_layer), "%s", msgs[nextstep].text);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "tc: new step: %s (%d)", msgs[nextstep].text, nextstep_countdown);
+    if (nextstep < msgs_size-1)
+      snprintf(s_nextstep_layer, sizeof(s_nextstep_layer), "Next:\n%s", msgs[nextstep+1].text);
+    else
+      snprintf(s_nextstep_layer, sizeof(s_nextstep_layer), "Next:\nDone!");
+    layer_mark_dirty(text_layer_get_layer(step_layer));
+    layer_mark_dirty(text_layer_get_layer(nextstep_layer));
+  }
   if (nextstep_countdown == 1)
     light_enable_interaction();
-	snprintf(s_countdown_layer, sizeof(s_countdown_layer), "%d", nextstep_countdown);
-	layer_mark_dirty(text_layer_get_layer(countdown_layer));
-	nextstep_countdown --;
-	if (nextstep_countdown == 0)
-		nextstep ++;
+  snprintf(s_countdown_layer, sizeof(s_countdown_layer), "%d", nextstep_countdown);
+  layer_mark_dirty(text_layer_get_layer(countdown_layer));
+  nextstep_countdown --;
+  if (nextstep_countdown == 0)
+    nextstep ++;
 }
 
 static void
 select_click_handler(ClickRecognizerRef recognizer, void *context)
 {
-	nextstep=0;
-	nextstep_countdown = 0;
-	if (tooth_timer != NULL)
-	{
-		app_timer_cancel(tooth_timer);
-		tooth_timer = NULL;
-	}
-	timer_callback(NULL);
+  nextstep=0;
+  nextstep_countdown = 0;
+  if (tooth_timer != NULL)
+  {
+    app_timer_cancel(tooth_timer);
+    tooth_timer = NULL;
+  }
+  timer_callback(NULL);
 }
 
 static void 
@@ -127,33 +127,33 @@ click_config_provider(void *context)
 static void 
 window_load(Window *window)
 {
-	Layer *window_layer = window_get_root_layer(window);
-	GRect bounds = layer_get_bounds(window_layer);
-	
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "window load. bounds.size w:%d, h:%d", bounds.size.w, bounds.size.h);
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "window load. bounds.size w:%d, h:%d", bounds.size.w, bounds.size.h);
 
-	s_step_layer[0] = 0;
-	step_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, 48 } });
-	text_layer_set_text(step_layer, s_step_layer);
-	text_layer_set_font(step_layer, fonts_get_system_font(TEXT_FONT));
-	// text_layer_set_text_alignment(step_layer, GTextAlignmentCenter);
-	layer_add_child(window_layer, text_layer_get_layer(step_layer));
-	
-	strcpy(s_countdown_layer, "Start");
-	countdown_layer = text_layer_create((GRect) { .origin = { 0, 48 }, .size = { bounds.size.w, 42 } });
-	text_layer_set_text(countdown_layer, s_countdown_layer);
-	text_layer_set_font(countdown_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-	text_layer_set_text_alignment(countdown_layer, GTextAlignmentCenter);
-	layer_add_child(window_layer, text_layer_get_layer(countdown_layer));
-	
-	s_nextstep_layer[0] = 0;
+  s_step_layer[0] = 0;
+  step_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, 48 } });
+  text_layer_set_text(step_layer, s_step_layer);
+  text_layer_set_font(step_layer, fonts_get_system_font(TEXT_FONT));
+  // text_layer_set_text_alignment(step_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(step_layer));
+  
+  strcpy(s_countdown_layer, "Start");
+  countdown_layer = text_layer_create((GRect) { .origin = { 0, 48 }, .size = { bounds.size.w, 42 } });
+  text_layer_set_text(countdown_layer, s_countdown_layer);
+  text_layer_set_font(countdown_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(countdown_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(countdown_layer));
+  
+  s_nextstep_layer[0] = 0;
   // nextstep is two lines (first is "Next:"), so needs to be taller than step
-	nextstep_layer = text_layer_create((GRect) { .origin = { 0, 90 }, .size = { bounds.size.w, 54 } });
-	text_layer_set_text(nextstep_layer, s_nextstep_layer);
-	text_layer_set_font(nextstep_layer, fonts_get_system_font(TEXT_FONT));
-	//text_layer_set_text_alignment(nextstep_layer, GTextAlignmentCenter);
-	layer_add_child(window_layer, text_layer_get_layer(nextstep_layer));
-	
+  nextstep_layer = text_layer_create((GRect) { .origin = { 0, 90 }, .size = { bounds.size.w, 54 } });
+  text_layer_set_text(nextstep_layer, s_nextstep_layer);
+  text_layer_set_font(nextstep_layer, fonts_get_system_font(TEXT_FONT));
+  //text_layer_set_text_alignment(nextstep_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(nextstep_layer));
+  
 }
 
 static void 
